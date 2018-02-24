68c6c286cd07fb6f1c969c6b59829937550e0c84
PascalBreno <brenodml@live.com>
2018-02-24 03:05:17 -0400

    Adicionado elemento correto
    Estava com o programa errado! Hahaha

--- Exercicio4/Grafos2.c
+++ Exercicio4/Grafos2.c
@@ -1,52 +1,104 @@
+/*
+* Grupo: Breno Felix e Victor Felix
+*
+*
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <iso646.h>
+#include <stdbool.h>
+//Cria as matrizes na memória
+bool **alocarMatrizQuadrada(int tamanho){
+    bool **fim = (bool**)malloc(tamanho* sizeof(bool*));
+
+    for (int i = 0; i < tamanho ; ++i) {
+        fim[i] = (bool*)calloc(tamanho, sizeof(bool));
+    }
+
+    return fim;
+}
+
+//Multiplica as matrizes com as funções propriamente ditas
+bool **multiplicarMatriz(bool **matriz1, bool **matriz2, int tamanho){
+    bool **fim, aux;
+    fim = alocarMatrizQuadrada(tamanho);
+
+
+    for (int i = 0; i < tamanho; ++i) {
+        for (int j = 0; j < tamanho; ++j) {
+            aux = false;
+            for (int k = 0; k < tamanho; ++k) {
+                aux = aux or (matriz1[i][k] and matriz2[k][j]);
+            }
+
+            fim[i][j] = aux;
+        }
+    }
+
+
+    return fim;
+}
+//Função para imprimir a Matriz
+void imprimirMatriz(bool **matriz, int tamanho){
+    for (int i = 0; i < tamanho; ++i) {
+        printf("| ");
+        for (int j = 0; j < tamanho; ++j) {
+            if(j+1<tamanho){
+                printf("%d -", matriz[i][j]);
+            }else{
+                printf(" %d |", matriz[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+//Algoritmo de Warshall aplicando ponteiro com endereço já alocado
 bool **warshall(bool **matriz, int tamanho){
-    bool **retorno;
-    retorno = alocarMatrizQuadrada(tamanho);
+    bool **fim;
+    fim = alocarMatrizQuadrada(tamanho);
     for(int k = 0; k<tamanho; k++){
         for(int i=0; i<tamanho; i++){
             for(int j=0; j<tamanho; j++){
-                retorno[i][j] = matriz[i][j] or (matriz[i][k] and matriz[k][j]);
+                fim[i][j] = matriz[i][j] or (matriz[i][k] and matriz[k][j]);
             }
         }
 
     }
 
-    return retorno;
+    return fim;
 }
 
-int main(){
-    int arestas;
-    do{
-        printf("Digite o número de vértices da matriz \n");
-        scanf("%d", &arestas);
-        if(arestas<2){
-            printf("Número digitado inválido, tente novamente! \n");
+int main() {
+    bool **inicio, **fim,**aux;
+    int tamanho, vertice1, vertice2;
+    vertice1 = 0;
+    vertice2 = 0;
+    //Essa parte é responsável por ler o número de arestas para saber o tamanho da matriz
+    printf("Insira o número de arestas total: \n");
+    scanf("%d", &tamanho);
+    //Função para a criação da matriz solicitada
+    inicio= alocarMatrizQuadrada(tamanho);
+    printf("Digite as arestas até um número menor que 0\n");
+    while(true){
+        scanf("%d%d", &vertice1, &vertice2);
+        if(vertice1 <0 or vertice2 < 0){
+            break;
         }
-    }while(arestas<2);
-    int matriz[arestas][arestas];
-    for(int j=0; j<arestas;j++){
-        printf("Linha nº %d \n", j+1);
-        for (int i=0; i<arestas; i++){
-            scanf("%d", &matriz[j][i]);
-        }
-    }
-    for(int j=0; j<arestas;j++){
-        printf("Linha nº %d : |", j+1);
-        for (int i=0; i<arestas; i++){
-            if(i+1<arestas){
-                printf("%d -", matriz[j][i]);
-            }else{
-                printf("%d", matriz[j][i]);
-            }
-        }
-        printf("|\n");
-    }
-    for (int i = 0; i < arestas-1; ++i) {
-        retorno = multiplicarMatriz(matriz, tamanho);
-        printf("Verificação de caminhos de tamanho %d \n", i+2);
-        imprimirMatriz(retorno, tamanho);
+        inicio[vertice1-1][vertice2-1] = true;
     }
 
+    //Processo de verificar os caminhos...
+    aux = inicio;
+    for (int i = 0; i < tamanho-1; ++i) {
+        aux = multiplicarMatriz(inicio, aux, tamanho);
+        printf("============= %d Etapa/Matriz =============\n", i+1);
+        imprimirMatriz(aux, tamanho);
+    }
+    //Aplicando o Warshall
+    aux = warshall(inicio, tamanho);
+    printf("============= Warshall ============= \n");
+    imprimirMatriz(aux, tamanho);
     return 0;
 }
