64fd3782b49cde257a82a3f371d67daf058da95b
PascalBreno <brenodml@live.com>
2018-02-24 03:01:35 -0400

    Programa finalizado
    Programa finalizado com o código do Gabriel.

--- Exercicio4/Grafos2.c
+++ Exercicio4/Grafos2.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+bool **warshall(bool **matriz, int tamanho){
+    bool **retorno;
+    retorno = alocarMatrizQuadrada(tamanho);
+    for(int k = 0; k<tamanho; k++){
+        for(int i=0; i<tamanho; i++){
+            for(int j=0; j<tamanho; j++){
+                retorno[i][j] = matriz[i][j] or (matriz[i][k] and matriz[k][j]);
+            }
+        }
+
+    }
+
+    return retorno;
+}
 
 int main(){
     int arestas;
@@ -14,7 +28,7 @@ int main(){
     for(int j=0; j<arestas;j++){
         printf("Linha nº %d \n", j+1);
         for (int i=0; i<arestas; i++){
-            scanf("%d", &matriz[j][i]);    
+            scanf("%d", &matriz[j][i]);
         }
     }
     for(int j=0; j<arestas;j++){
@@ -24,9 +38,15 @@ int main(){
                 printf("%d -", matriz[j][i]);
             }else{
                 printf("%d", matriz[j][i]);
-            }    
+            }
         }
         printf("|\n");
     }
+    for (int i = 0; i < arestas-1; ++i) {
+        retorno = multiplicarMatriz(matriz, tamanho);
+        printf("Verificação de caminhos de tamanho %d \n", i+2);
+        imprimirMatriz(retorno, tamanho);
+    }
+
     return 0;
 }
